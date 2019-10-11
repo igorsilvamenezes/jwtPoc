d50518722d7e59758ecb1808d20b38c8be378d63
igorsilvamenezes <igorsilvamenezes@gmail.com>
2019-10-11 19:02:01 -0300

    Initial commit

---
+++ src/main/java/br/com/meltha/jwtPoc/JwtPocApplication.java
@@ -0,0 +1,13 @@
+package br.com.meltha.jwtPoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtPocApplication.class, args);
+	}
+
+}
